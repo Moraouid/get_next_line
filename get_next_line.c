b20c65c2490d9b78dffcb1862fc9b535dba8be8e
Soufiane El Abboubi <sel-abbo@c2r6p5.1337.ma>
2024-12-12 18:10:54 +0100

    1337

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sel-abbo <sel-abbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 18:20:31 by sel-abbo          #+#    #+#             */
-/*   Updated: 2024/12/09 18:24:36 by sel-abbo         ###   ########.fr       */
+/*   Updated: 2024/12/10 20:55:27 by sel-abbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,24 @@ static char	*read_join(int fd, char *rem)
 	char	*BUFFER;
 	ssize_t	b_read;
 	
+	b_read = 1;
 	if(BUFFER_SIZE < 0)
-		return NULL;
-	BUFFER = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
-	b_read = read(fd, BUFFER, BUFFER_SIZE);
-	if (b_read < 0)
-		return (free(rem), free(BUFFER), BUFFER = NULL, NULL);
-	if (b_read == 0)
-	{
-		free(BUFFER);
-		BUFFER = NULL;
-		return (rem);
-	}
-	if (!rem)
-		 rem = ft_strdup("");
-	BUFFER[b_read] = '\0';
-	rem = ft_strjoin(rem, BUFFER);
-	free(BUFFER);
-	BUFFER = NULL;
-	if (!rem)
 		return (NULL);
+	while (b_read > 0 && !ft_strchr(rem, '\n'))
+	{
+		BUFFER = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		if (!BUFFER)
+			return (free(rem), NULL);
+		b_read = read(fd, BUFFER, BUFFER_SIZE);
+		if (b_read < 0)
+			return (free(rem), free(BUFFER),  NULL);
+	
+		if (b_read == 0)
+			return (free(BUFFER), rem);
+		BUFFER[b_read] = '\0';
+		rem = ft_strjoin(rem, BUFFER);
+		free(BUFFER);
+	}
 	return (rem);
 }
 
@@ -60,7 +58,7 @@ static char	*line_extra(char	**rem)
 	}
 	free(*rem);
 	*rem = tmp;
-	return (line);	
+	return (line);
 }
 
 char	*get_next_line(int fd)
@@ -70,8 +68,8 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-//	if (!rem)
-//		rem = ft_strdup("");
+	if (!rem)
+		 rem = ft_strdup("");
 	rem = read_join(fd, rem);
 	if (!rem)
 		return (NULL);
@@ -88,26 +86,36 @@ char	*get_next_line(int fd)
 	return (free(rem), rem = NULL, NULL);
 }
 
-// #include <fcntl.h>
-// #include <stdio.h>
+#include <fcntl.h>
+#include <stdio.h>
 
-// int main()
-// {
-//     int fd = open("test.txt", O_RDONLY); 
-//      if (fd < 0)
-//      {
-//          printf("Error: Could not open file.\n");
-//          return (1);
-//      }
+int main()
+{
+    int fd = open("test.txt", O_RDONLY); 
+     if (fd < 0)
+     {
+         printf("Error: Could not open file.\n");
+         return (1);
+     }
 
-//     char *line;
-//     while ((line = get_next_line(fd)) != NULL)
-//     {
-//         printf("%s", line);
-//         free(line);
-// 		//line = NULL;
-//     }
+    char *line;
+	// line = get_next_line(fd);
+	// printf("%s", line);
+	// line = get_next_line(fd);
+	// printf("%s", line);
+	// line = get_next_line(fd);
+	// printf("%s", line);
+	// line = get_next_line(fd);
+	// printf("%s", line);
+	// line = get_next_line(fd);
+	// printf("%s", line);
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        printf("%s", line);
+        free(line);
+		//line = NULL;
+    }
 
-//     close(fd);
-//     return (0);
-// }
+    close(fd);
+    return (0);
+}
